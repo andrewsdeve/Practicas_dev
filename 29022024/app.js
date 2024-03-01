83bdde289bad14f904f5a0d051efea4b1cce4584
andrewsdeve <89328500+andrewsdeve@users.noreply.github.com>
2024-02-29 21:50:01 -0500

    SegundoEjercicio

--- 29022024/app.js
+++ 29022024/app.js
@@ -7,5 +7,17 @@ function Empleados(nombres,apellidos,edad,cargo){
         console.log(`Hola me llamo ${nombres} ${apellidos}, tengo ${edad} anios y actualmente ocupo el cargo ${cargo}`);
     }
 }
-
 let empleado1 = new Empleados("Juan", "Pelufo", 33, "Gerente");
+
+function Automovil(marca,modelo,anio,color){
+    this.marca = marca;
+    this.modelo = modelo;
+    this.anio = anio;
+    this.color = color;
+    this.mensaje = function(){
+        console.log(`Bienvenido a la presentacion del coche ${marca} del ${modelo} que fue fabricado en ${anio} y que el ${color}, es el favorito de todos`)
+    }
+}
+
+let carro1 = new Automovil("Chevrolet","Corsa", 2024, "Rojo");
+
